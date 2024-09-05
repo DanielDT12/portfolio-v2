9d15082ae6a2b361db805734cba8841e630e0145
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-05 21:51:14 +0200

    add data list for menu

---
+++ src/data/MENU_LIST.js
@@ -0,0 +1,16 @@
+const MENU_LIST = [
+	{
+		name: "About",
+		href: "/about",
+	},
+	{
+		name: "My Work",
+		href: "/work",
+	},
+	{
+		name: "Contact",
+		href: "/contact",
+	},
+];
+
+export default MENU_LIST;
