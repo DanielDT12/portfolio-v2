2e59964fe3c31c9f2e2190c52a367986bcde261f
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-09 12:56:08 +0200

    removed utility class ul

--- src/components/smallComponents/PrimaryNav.jsx
+++ src/components/smallComponents/PrimaryNav.jsx
@@ -4,7 +4,7 @@ import MENU_LIST from "../../data/MENU_LIST";
 export const PrimaryNav = () => {
 	return (
 		<nav className="primary-nav">
-			<ul className="primary-nav__list">
+			<ul className="primary-nav__list flex-col">
 				{MENU_LIST.map((link, index) => (
 					<li key={link.name + index} className="primary-nav__list-item">
 						<Link className="primary-nav__link" to={link.href}>
