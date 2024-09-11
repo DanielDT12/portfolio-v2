ab36662e9af06cca2fe0818200ac7ae78d4b187c
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-11 11:42:29 +0200

    add context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { PrimaryHeader } from "./components/PrimaryHeader";
 import { PrimaryFooter } from "./components/PrimaryFooter";
+import AppContextProvider from "./context/appContext";
 
 function App() {
 	return (
-		<div className="app-wrapper">
-			<PrimaryHeader
-				headerClass={"primary-header flex"}
-				themeSwitcherClass={"primary-header__theme-switcher"}
-			/>
-			<Outlet />
-			<PrimaryFooter footerClass={"primary-footer flex"} />
-		</div>
+		<AppContextProvider>
+			<div className="app-wrapper">
+				<PrimaryHeader
+					headerClass={"primary-header flex"}
+					themeSwitcherClass={"primary-header__theme-switcher"}
+				/>
+				<Outlet />
+				<PrimaryFooter footerClass={"primary-footer flex"} />
+			</div>
+		</AppContextProvider>
 	);
 }
 
