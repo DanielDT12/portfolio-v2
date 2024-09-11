b1554bb699ea19a1f43421a9e49b75616b9bb26f
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-11 11:42:42 +0200

    setup basic useContext

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,10 +1,15 @@
 import { createContext, useState } from "react";
 
 export const AppContext = createContext({
-	open: false,
+	isOpen: false,
+	setIsOpen: () => {},
 });
 
 export default function AppContextProvider({ children }) {
 	const [isOpen, setIsOpen] = useState(false);
-	return <AppContext.Provider value={{ open }}>{children}</AppContext.Provider>;
+	return (
+		<AppContext.Provider value={{ isOpen, setIsOpen }}>
+			{children}
+		</AppContext.Provider>
+	);
 }
