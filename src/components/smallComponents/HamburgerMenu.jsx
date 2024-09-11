d7e23629cd54c34bc96dda3f87c21086b854ef54
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-11 11:42:55 +0200

    changed useState to useContext

--- src/components/smallComponents/HamburgerMenu.jsx
+++ src/components/smallComponents/HamburgerMenu.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoCloseSharp } from "react-icons/io5";
 import { PrimaryNav } from "./PrimaryNav";
+import { AppContext } from "../../context/appContext";
 
 export default function HamburgerMenu({
 	wrapperClass,
 	buttonClass,
 	iconClass,
 }) {
-	const [isOpen, setIsOpen] = useState(false);
+	const { isOpen, setIsOpen } = useContext(AppContext);
 
 	const handleChange = () => {
 		return setIsOpen(!isOpen);
