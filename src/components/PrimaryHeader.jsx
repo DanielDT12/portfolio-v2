76543606da5788d8875fc11de66a2f542a5e4a99
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-13 14:09:45 +0200

    add breadcrumb component

--- src/components/PrimaryHeader.jsx
+++ src/components/PrimaryHeader.jsx
@@ -1,8 +1,10 @@
+import Breadcrumb from "./smallComponents/Breadcrumb";
 import { DarkLightModeToggle } from "./smallComponents/DarkLightModeToggle";
 
 export const PrimaryHeader = ({ headerClass }) => {
 	return (
 		<header className={headerClass}>
+			<Breadcrumb />
 			<DarkLightModeToggle
 				wrapperClass={"theme-switcher flex"}
 				labelClass={"theme-switcher__label"}
