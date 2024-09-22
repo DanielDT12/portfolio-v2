50123a4f5d514a4be3042145c840ee306dd53ac8
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-23 01:01:36 +0200

    add breadcrumb component

---
+++ src/components/UI/Breadcrumb.jsx
@@ -0,0 +1,24 @@
+import { Link, useLocation } from "react-router-dom";
+
+export default function Breadcrumb() {
+	const location = useLocation();
+
+	const pathName = location.pathname.slice(1, location.pathname.length);
+
+	return (
+		<div className="breadcrumb">
+			<Link className="breadcrumb__link" to={"/"}>
+				{"home"}
+			</Link>
+			<span>{` / `}</span>
+			{location.pathname !== "/" && (
+				<Link
+					className="breadcrumb__link breadcrumb__link--active-page"
+					to={`${location.pathname}`}
+				>
+					{pathName}
+				</Link>
+			)}
+		</div>
+	);
+}
