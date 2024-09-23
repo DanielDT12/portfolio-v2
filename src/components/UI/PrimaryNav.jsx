75547e322513e4c0127f76e06ab84a1ed95037e8
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-23 08:41:37 +0200

    add onclick event to link

--- src/components/UI/PrimaryNav.jsx
+++ src/components/UI/PrimaryNav.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 import MENU_LIST from "../../data/MENU_LIST";
 
-export const PrimaryNav = ({ navClass, ulClass, liClass, linkClass }) => {
+export const PrimaryNav = ({
+	navClass,
+	ulClass,
+	liClass,
+	linkClass,
+	linkOnClick,
+}) => {
 	return (
 		<nav className={navClass}>
 			<ul className={ulClass}>
 				{MENU_LIST.map((link, index) => (
 					<li key={link.name + index} className={liClass}>
-						<Link className={linkClass} to={link.href}>
+						<Link className={linkClass} to={link.href} onClick={linkOnClick}>
 							{link.name}
 						</Link>
 					</li>
