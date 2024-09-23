4f8b9ab725d8a1c04caeb1dd26609c0556985fdb
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-09-23 08:05:29 +0200

    add work card list array

---
+++ src/data/WORK_CARD_LIST.js
@@ -0,0 +1,19 @@
+export const WORK_CARD_LIST = [
+	{
+		title: "Frontend Mentor",
+		imgSrc: "/images/fem-bg-1.jpeg",
+		infoText:
+			"Frontend mentor project i have done, made with the help of a design file and design system.",
+	},
+	{
+		title: "Portfolio Page",
+		imgSrc: "/images/portfolio-img.jpg",
+		infoText:
+			"My first personal site, its built using react, and a few npm packages.",
+	},
+	{
+		title: "Pokédex",
+		imgSrc: "/images/pokedex-img.jpg",
+		infoText: "Pokédex project i am working on using the pokeApi.",
+	},
+];
