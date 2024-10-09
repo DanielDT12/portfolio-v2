01b42f3a3d66aacefd5f561bfc90e62f406a0962
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-10-09 12:09:23 +0200

    add href

--- src/data/WORK_CARD_LIST.js
+++ src/data/WORK_CARD_LIST.js
@@ -4,16 +4,24 @@ export const WORK_CARD_LIST = [
 		imgSrc: "/images/fem-bg-1.jpeg",
 		infoText:
 			"Frontend mentor project i have done, made with the help of a design file and design system.",
+		hrefCode:
+			"https://github.com/DanielDT12/social-media-dashboard-with-theme-switcher-master",
+		hrefPreview:
+			"https://danieldt12.github.io/social-media-dashboard-with-theme-switcher-master/",
 	},
 	{
 		title: "Portfolio Page",
 		imgSrc: "/images/portfolio-img.jpg",
 		infoText:
 			"My first personal site, its built using react, and a few npm packages.",
+		hrefCode: "https://github.com/DanielDT12/portfolio-v2",
+		hrefPreview: "https://portfolio-v2-one-jet.vercel.app/",
 	},
 	{
 		title: "Pokédex",
 		imgSrc: "/images/pokedex-img.jpg",
 		infoText: "Pokédex project i am working on using the pokeApi.",
+		hrefCode: "https://github.com/DanielDT12/pokedex-v2-react",
+		hrefPreview: "https://pokedex-v2-react.vercel.app/",
 	},
 ];
