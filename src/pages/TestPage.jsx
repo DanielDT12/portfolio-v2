60cb125f7cab00f67ee92b1e98c5c76ececd5f94
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-11-28 15:00:19 +0100

    add test page for testing components

---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,9 @@
+import MyWorkCarousel from "../components/MyWorkCarousel";
+
+export default function TestPage() {
+	return (
+		<div style={{ display: "grid", placeContent: "center", height: "94vh" }}>
+			<MyWorkCarousel />
+		</div>
+	);
+}
