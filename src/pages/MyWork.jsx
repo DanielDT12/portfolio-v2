248816091638979cdc2b18bc0ccf8b23ab9d6b18
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-11-29 12:04:44 +0100

    added component

--- src/pages/MyWork.jsx
+++ src/pages/MyWork.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import HamburgerMenu from "../components/UI/HamburgerMenu";
-import { MyWorkCard } from "../components/MyWorkCard";
-import { WORK_CARD_LIST } from "../data/WORK_CARD_LIST";
+import MyWorkCardsContainer from "../components/MyWorkCarousel";
 
 export const MyWork = () => {
 	return (
@@ -14,22 +13,7 @@ export const MyWork = () => {
 			<div className="my-work">
 				<h2 className="my-work__title">My Work</h2>
 				<p>Here are some projects i have worked on.</p>
-				<div className="my-work__card-wrapper">
-					{WORK_CARD_LIST.map((card, index) => (
-						<MyWorkCard
-							key={card.title + index}
-							cardClass={"my-work-card"}
-							imgClass={"my-work-card__image"}
-							imgSrc={card.imgSrc}
-							titleClass={"my-work-card__title"}
-							cardTextClass={"my-work-card__text"}
-							hrefCode={card.hrefCode}
-							hrefPreview={card.hrefPreview}
-							titleText={card.title}
-							cardText={card.infoText}
-						/>
-					))}
-				</div>
+				<MyWorkCardsContainer />
 			</div>
 			<Link className={"home-link"} to={"/"}>
 				&larr; Home
